db1285bf7f19335bd32e069bed3260bd9bc3874b
Ayush Goel <108970801+ayushgoel-01@users.noreply.github.com>
2025-01-16 14:49:19 +0530

    Time: 15 ms (18.72%) | Memory: 275.7 MB (21.18%) - LeetSync

--- 2792-neighboring-bitwise-xor/neighboring-bitwise-xor.cpp
+++ 2792-neighboring-bitwise-xor/neighboring-bitwise-xor.cpp
@@ -2,8 +2,31 @@ class Solution {
 public:
     bool doesValidArrayExist(vector<int>& derived) {
         int n = derived.size();
-        int ans = 0;
-        for(auto i: derived) ans = ans ^ i;
-        return ans == 0;
+
+        vector<int> v(n);
+        v[0] = 0;
+        for(int i=1; i<n; i++){
+            v[i] = v[i-1] ^ derived[i-1];
+        }
+
+        if(v[0] ^ v[n-1] == derived[n-1]) return true;
+
+        v[0] = 1;
+        for(int i=1; i<n; i++){
+            v[i] = v[i-1] ^ derived[i-1];
+        }
+
+        return (v[0] ^ v[n-1]) == derived[n-1];
     }
 };
+
+
+
+// class Solution {
+// public:
+//     bool doesValidArrayExist(vector<int>& derived) {
+//         int ans = 0;
+//         for(auto i: derived) ans = ans ^ i;
+//         return ans == 0;
+//     }
+// };
