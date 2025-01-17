a499d3a1a656aff0e7f91951e7f51e1907d3f21b
Ayush Goel <108970801+ayushgoel-01@users.noreply.github.com>
2025-01-17 09:44:20 +0530

    Time: 1 ms (70.56%) | Memory: 264 MB (48.13%) - LeetSync

--- 2792-neighboring-bitwise-xor/neighboring-bitwise-xor.cpp
+++ 2792-neighboring-bitwise-xor/neighboring-bitwise-xor.cpp
@@ -1,22 +1,9 @@
 class Solution {
 public:
     bool doesValidArrayExist(vector<int>& derived) {
-        int n = derived.size();
-
-        vector<int> v(n);
-        v[0] = 0;
-        for(int i=1; i<n; i++){
-            v[i] = v[i-1] ^ derived[i-1];
-        }
-
-        if(v[0] ^ v[n-1] == derived[n-1]) return true;
-
-        v[0] = 1;
-        for(int i=1; i<n; i++){
-            v[i] = v[i-1] ^ derived[i-1];
-        }
-
-        return (v[0] ^ v[n-1]) == derived[n-1];
+        int ans = 0;
+        for(auto i: derived) ans = ans ^ i;
+        return ans == 0;
     }
 };
 
@@ -25,8 +12,21 @@ public:
 // class Solution {
 // public:
 //     bool doesValidArrayExist(vector<int>& derived) {
-//         int ans = 0;
-//         for(auto i: derived) ans = ans ^ i;
-//         return ans == 0;
+//         int n = derived.size();
+
+//         vector<int> v(n);
+//         v[0] = 0;
+//         for(int i=1; i<n; i++){
+//             v[i] = v[i-1] ^ derived[i-1];
+//         }
+
+//         if(v[0] ^ v[n-1] == derived[n-1]) return true;
+
+//         v[0] = 1;
+//         for(int i=1; i<n; i++){
+//             v[i] = v[i-1] ^ derived[i-1];
+//         }
+
+//         return (v[0] ^ v[n-1]) == derived[n-1];
 //     }
 // };
