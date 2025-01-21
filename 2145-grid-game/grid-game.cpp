364b86a5ca9a469f00cbd78a9fbc2cc531441852
Ayush Goel <108970801+ayushgoel-01@users.noreply.github.com>
2025-01-21 10:27:52 +0530

    Time: 0 ms (100.00%) | Memory: 66.7 MB (76.92%) - LeetSync

--- 2145-grid-game/grid-game.cpp
+++ 2145-grid-game/grid-game.cpp
@@ -4,25 +4,20 @@ public:
     long long gridGame(vector<vector<int>>& grid) {
         int n = grid.size();
         int m = grid[0].size();
+
         // Total sum of row 1
         ll sum1 = accumulate(grid[0].begin(),grid[0].end(),0LL);
+        ll sum2 = 0;
 
-        vector<ll> v(m,0);          // Prefix sum for row 2
-        v[0] = grid[1][0];
-        for(int j=1; j<m; j++){
-            v[j] = v[j-1] + grid[1][j];
-        }
-
-        ll mini = LLONG_MAX;
-        ll currSum = 0;
+        ll ans = LLONG_MAX;
         for(int j=0; j<m; j++){
-            currSum += grid[0][j];
+            sum1 -= grid[0][j];
 
-            ll rem = sum1-currSum;
-            if(j-1 >= 0) rem = max(rem,v[j-1]);
+            ll rem = max(sum1,sum2);
+            ans = min(ans,rem);
 
-            mini = min(mini,rem);
+            sum2 += grid[1][j];
         }
-        return mini;
+        return ans;
     }
 };
