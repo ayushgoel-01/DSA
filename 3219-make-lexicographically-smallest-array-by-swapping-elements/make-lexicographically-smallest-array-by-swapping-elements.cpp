017912d4d6efb218b048aa6887d4afec638f06f8
Ayush Goel <108970801+ayushgoel-01@users.noreply.github.com>
2025-01-25 14:40:06 +0530

    Time: 335 ms (29.73%) | Memory: 234 MB (25.00%) - LeetSync

--- 3219-make-lexicographically-smallest-array-by-swapping-elements/make-lexicographically-smallest-array-by-swapping-elements.cpp
+++ 3219-make-lexicographically-smallest-array-by-swapping-elements/make-lexicographically-smallest-array-by-swapping-elements.cpp
@@ -1,23 +1,22 @@
-class Solution {
+class Solution {            // Using Doubly Linked List
 public:
     vector<int> lexicographicallySmallestArray(vector<int>& nums, int limit) {
         int n = nums.size();
-
         vector<int> v(nums.begin(),nums.end());
         sort(v.begin(),v.end());
 
-        unordered_map<int,queue<int>> mp;
-        unordered_map<int,int> elToGrp;
+        unordered_map<int,list<int>> mp;       // Grp -> element mapping , O(1) using DLL
+        unordered_map<int,int> elToGrp;         // element -> Grp mapping
 
         int grp = 1;
-        mp[grp].push(v[0]);
+        mp[grp].push_back(v[0]);
         elToGrp[v[0]] = grp;
 
         for(int i=1; i<n; i++){
-            if(v[i] - v[i-1] > limit){
+            if(v[i] - v[i-1] > limit){          // Change thr group
                 grp++;
             }
-            mp[grp].push(v[i]);
+            mp[grp].push_back(v[i]);
             elToGrp[v[i]] = grp;
         }
 
@@ -26,10 +25,48 @@ public:
             int el = nums[i];
             int grp = elToGrp[el];
 
-            int val = mp[grp].front();
-            mp[grp].pop();
+            int val = *mp[grp].begin();          // Find Smallest element of that group
+            mp[grp].pop_front();
             ans[i] = val;
         }
         return ans;
     }
 };
+
+
+
+
+// class Solution {            // Using Queue
+// public:
+//     vector<int> lexicographicallySmallestArray(vector<int>& nums, int limit) {
+//         int n = nums.size();
+//         vector<int> v(nums.begin(),nums.end());
+//         sort(v.begin(),v.end());
+
+//         unordered_map<int,queue<int>> mp;       // Grp -> element mapping
+//         unordered_map<int,int> elToGrp;         // element -> Grp mapping
+
+//         int grp = 1;
+//         mp[grp].push(v[0]);
+//         elToGrp[v[0]] = grp;
+
+//         for(int i=1; i<n; i++){
+//             if(v[i] - v[i-1] > limit){          // Change thr group
+//                 grp++;
+//             }
+//             mp[grp].push(v[i]);
+//             elToGrp[v[i]] = grp;
+//         }
+
+//         vector<int> ans(n,0);
+//         for(int i=0; i<n; i++){
+//             int el = nums[i];
+//             int grp = elToGrp[el];
+
+//             int val = mp[grp].front();          // Find Smallest element of that group
+//             mp[grp].pop();
+//             ans[i] = val;
+//         }
+//         return ans;
+//     }
+// };
