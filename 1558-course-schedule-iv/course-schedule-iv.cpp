ab42e23311673809dbfe64d9b4c9870a658e3130
Ayush Goel <108970801+ayushgoel-01@users.noreply.github.com>
2025-01-27 09:39:36 +0530

    Time: 202 ms (17.35%) | Memory: 159 MB (7.19%) - LeetSync

---
+++ 1558-course-schedule-iv/course-schedule-iv.cpp
@@ -0,0 +1,44 @@
+class Solution {
+private:
+    bool solve(int src, int target, unordered_map<int,vector<int>>& mp, int n){
+        vector<bool> vis(n,false);
+        vis[src] = true;
+
+        queue<int> q;
+        q.push(src);
+        while(!q.empty()){
+            auto node = q.front();
+            q.pop();
+
+            for(auto it: mp[node]){
+                if(!vis[it]){
+                    vis[it] = true;
+                    q.push(it);
+                    if(it == target) return true;
+                }
+            }
+        }
+        return false;
+    }
+public:
+    vector<bool> checkIfPrerequisite(int numCourses, vector<vector<int>>& prerequisites, vector<vector<int>>& queries) {
+        int n = numCourses;
+        int m = queries.size();
+
+        unordered_map<int,vector<int>> mp;
+        for(auto it: prerequisites){
+            int u = it[0];
+            int v = it[1];
+            mp[u].push_back(v);
+        }
+
+        vector<bool> ans(m,false);
+        for(int i=0; i<m; i++){
+            int u = queries[i][0];
+            int v = queries[i][1];
+            
+            if(solve(u,v,mp,n)) ans[i] = true;
+        }
+        return ans;
+    }
+};
