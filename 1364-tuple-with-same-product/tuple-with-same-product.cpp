f69a75b3f7e71869aaa2ad36c7fd8e57731e7e92
Ayush Goel <108970801+ayushgoel-01@users.noreply.github.com>
2025-02-06 11:47:20 +0530

    Time: 1871 ms (5.01%) | Memory: 10.7 MB (99.37%) - LeetSync

---
+++ 1364-tuple-with-same-product/tuple-with-same-product.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int tupleSameProduct(vector<int>& nums) {
+        int n = nums.size();
+        sort(nums.begin(),nums.end());
+
+        int ans = 0;
+        for(int i=0; i<n; i++){
+            for(int j=n-1; j>=0; j--){
+                if(i == j) continue;
+
+                int k = i+1, l = j-1;
+                if(i == k || j == k) k++;
+                if(i == l || j == l) l--;
+
+                while(k < l){
+                    long long a = nums[i] * nums[j];
+                    long long b = nums[k] * nums[l];
+
+                    if(a == b){
+                        ans++;
+                        k++;
+                    }
+                    else if(a > b) k++;
+                    else l--;
+                }
+            }
+        }
+        return ans * 8;
+    }
+};
