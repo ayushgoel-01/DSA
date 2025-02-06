83a96d547e8769b6050c8af52cf30023e4814917
Ayush Goel <108970801+ayushgoel-01@users.noreply.github.com>
2025-02-06 11:59:29 +0530

    Time: 2690 ms (5.01%) | Memory: 492.3 MB (6.27%) - LeetSync

--- 1364-tuple-with-same-product/tuple-with-same-product.cpp
+++ 1364-tuple-with-same-product/tuple-with-same-product.cpp
@@ -1,4 +1,4 @@
-class Solution {
+class Solution {            // Better Approach
 public:
     int tupleSameProduct(vector<int>& nums) {
         int n = nums.size();
@@ -7,25 +7,56 @@ public:
         int ans = 0;
         for(int i=0; i<n; i++){
             for(int j=n-1; j>i; j--){
-                if(i == j) continue;
+                long long prod = nums[i] * nums[j];
+                unordered_set<int> st;
 
-                int k = i+1, l = j-1;
-                if(i == k || j == k) k++;
-                if(i == l || j == l) l--;
+                for(int k=i+1; k<j; k++){
+                    if(prod % nums[k] == 0){
+                        int val = prod / nums[k];
 
-                while(k < l){
-                    long long a = nums[i] * nums[j];
-                    long long b = nums[k] * nums[l];
-
-                    if(a == b){
-                        ans++;
-                        k++;
+                        if(st.count(val) >= 1) ans++;
+                        st.insert(nums[k]);
                     }
-                    else if(a > b) k++;
-                    else l--;
                 }
             }
         }
-        return ans * 8;
+        return ans * 8;         // Every tuple can have 8 permutations
     }
 };
+
+
+
+
+
+
+// class Solution {            // Brute Force
+// public:
+//     int tupleSameProduct(vector<int>& nums) {
+//         int n = nums.size();
+//         sort(nums.begin(),nums.end());
+
+//         int ans = 0;
+//         for(int i=0; i<n; i++){
+//             for(int j=n-1; j>i; j--){
+//                 if(i == j) continue;
+
+//                 int k = i+1, l = j-1;
+//                 if(i == k || j == k) k++;
+//                 if(i == l || j == l) l--;
+
+//                 while(k < l){
+//                     long long a = nums[i] * nums[j];
+//                     long long b = nums[k] * nums[l];
+
+//                     if(a == b){
+//                         ans++;
+//                         k++;
+//                     }
+//                     else if(a > b) k++;
+//                     else l--;
+//                 }
+//             }
+//         }
+//         return ans * 8;         // Every tuple can have 8 permutations
+//     }
+// };
