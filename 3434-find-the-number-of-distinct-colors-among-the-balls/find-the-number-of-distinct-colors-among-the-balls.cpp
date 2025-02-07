866e6e33d11abdde9c611f31d9b2f25e1710c2d2
Ayush Goel <108970801+ayushgoel-01@users.noreply.github.com>
2025-02-07 10:14:24 +0530

    Time: 162 ms (38.91%) | Memory: 167.8 MB (30.87%) - LeetSync

--- 3434-find-the-number-of-distinct-colors-among-the-balls/find-the-number-of-distinct-colors-among-the-balls.cpp
+++ 3434-find-the-number-of-distinct-colors-among-the-balls/find-the-number-of-distinct-colors-among-the-balls.cpp
@@ -10,23 +10,25 @@ public:
         unordered_map<int,int> freq;
 
         for(int i=0; i<n; i++){
-            int x = queries[i][0];
-            int y = queries[i][1];
+            int x = queries[i][0];          // Ball
+            int y = queries[i][1];          // Color
 
-            if(mp.find(x) == mp.end()){
-                mp[x] = y;
+            if(mp.find(x) == mp.end()){     // If ball is not colored, color it & insert color in set
+                mp[x] = y;                  
                 st.insert(y);
             }
             else{
-                int prev = mp[x];
+                int prev = mp[x];            // If ball is already colored, find prev color
+
                 if(freq[prev] == 1) st.erase(prev);
-                freq[prev]--;
+                freq[prev]--;               // Re-Coloring
+
                 mp[x] = y;
                 st.insert(y);
             }
 
             ans.push_back((int)st.size());
-            freq[y]++;
+            freq[y]++;                      // Frequency of current color increases
         }
         return ans;
     }
