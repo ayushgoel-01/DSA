781d875db00170fc06b5f6e1d09ba3e3ac06b44f
Ayush Goel <108970801+ayushgoel-01@users.noreply.github.com>
2025-02-11 09:53:00 +0530

    Time: 0 ms (100.00%) | Memory: 9.4 MB (34.81%) - LeetSync

--- 2021-remove-all-occurrences-of-a-substring/remove-all-occurrences-of-a-substring.cpp
+++ 2021-remove-all-occurrences-of-a-substring/remove-all-occurrences-of-a-substring.cpp
@@ -1,25 +1,37 @@
 class Solution {
 public:
     string removeOccurrences(string s, string part) {
-        int m = part.size();
-
-        while(true){
-            int n = s.size();
-            int i = 0;
-            string temp = "";
-            bool got = false;
-
-            while(i < n){
-                if(!got && s.substr(i,m) == part){
-                    i += m;
-                    got = true;
-                }
-                else temp += s[i++];
-            }
-            s = temp;
-            if(!got) break;
-
+        while(s.length()!=0 && s.find(part)<s.length()){
+            s.erase(s.find(part), part.length());
         }
         return s;
     }
 };
+
+
+
+// class Solution {
+// public:
+//     string removeOccurrences(string s, string part) {
+//         int m = part.size();
+
+//         while(true){
+//             int n = s.size();
+//             int i = 0;
+//             string temp = "";
+//             bool got = false;
+
+//             while(i < n){
+//                 if(!got && s.substr(i,m) == part){
+//                     i += m;
+//                     got = true;
+//                 }
+//                 else temp += s[i++];
+//             }
+//             s = temp;
+//             if(!got) break;
+
+//         }
+//         return s;
+//     }
+// };
