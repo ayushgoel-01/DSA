84e1ef7254679c5318be92015276420b8355d343
Ayush Goel <108970801+ayushgoel-01@users.noreply.github.com>
2025-06-01 20:28:59 +0530

    Time: 31 ms (26.47%) | Memory: 9 MB (57.06%) - LeetSync

--- 3201-distribute-candies-among-children-ii/distribute-candies-among-children-ii.cpp
+++ 3201-distribute-candies-among-children-ii/distribute-candies-among-children-ii.cpp
@@ -1,12 +1,12 @@
 class Solution {
 public:
     long long distributeCandies(int n, int limit) {
-        int mini = max(0,n-2*limit);
+        int mini = max(0,n-2*limit); // Assing other 2 children limit candies, so remaining = n-2*limit
         int maxi = min(n,limit);
 
         long long ans = 0;
         for(int i=mini; i<=maxi; i++){
-            int N = n - i;
+            int N = n - i;          // Updated candies that are left
 
             int minch2 = max(0,N-limit);
             int maxch2 = min(N,limit);
