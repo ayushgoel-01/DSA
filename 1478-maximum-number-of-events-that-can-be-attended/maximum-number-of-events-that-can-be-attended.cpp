389a113aa845c381ca8980efff08e28256bfee15
Ayush Goel <108970801+ayushgoel-01@users.noreply.github.com>
2025-07-07 10:07:57 +0530

    Time: 51 ms (95.74%) | Memory: 74.8 MB (85.96%) - LeetSync

---
+++ 1478-maximum-number-of-events-that-can-be-attended/maximum-number-of-events-that-can-be-attended.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int maxEvents(vector<vector<int>>& events) {
+        int n = events.size();
+        sort(events.begin(),events.end());
+
+        int ans = 0, day = events[0][0];
+        int i = 0;
+        priority_queue<int,vector<int>,greater<int>> pq;
+
+        while(!pq.empty() || i < n){
+            while(i < n && events[i][0] == day){
+                pq.push(events[i][1]);
+                i++;
+            }
+
+            if(!pq.empty()){
+                pq.pop();
+                ans++;
+            }
+            day++;
+
+            while(!pq.empty() && pq.top() < day){
+                pq.pop();
+            }
+        }
+        return ans;
+    }
+};
