b1cf58f0c6816b85ca40827a53aa0bf1c2d11f47
Ayush Goel <108970801+ayushgoel-01@users.noreply.github.com>
2025-07-11 20:27:43 +0530

    Time: 75 ms (63.50%) | Memory: 98.2 MB (56.92%) - LeetSync

--- 2479-meeting-rooms-iii/meeting-rooms-iii.cpp
+++ 2479-meeting-rooms-iii/meeting-rooms-iii.cpp
@@ -5,6 +5,7 @@ public:
 
         sort(meetings.begin(),meetings.end());
 
+        // Keeps track when the ith room will be available
         vector<long long> lastUsed(n,0);
         vector<int> ans(n,0);
 
@@ -14,7 +15,7 @@ public:
             int duration = end - start;
             bool found = false;
 
-            long long earlyEndTime = LLONG_MAX;
+            long long earlyEndTime = LLONG_MAX;     // Track which room will get free early
             int earlyEndRoom = 0;
 
             for(int room=0; room<n; room++){
@@ -24,13 +25,13 @@ public:
                     found = true;
                     break;
                 }
-
+                // Update Earliest Free room available
                 if(lastUsed[room] < earlyEndTime){
                     earlyEndTime = lastUsed[room];
                     earlyEndRoom = room;
                 }
             }
-            if(!found){
+            if(!found){     // If no room is empty, then check for earliest free room available
                 lastUsed[earlyEndRoom] += duration;
                 ans[earlyEndRoom]++;
             }
