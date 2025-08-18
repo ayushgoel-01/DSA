d9d96761d54f1c4633bd38a8bfe0871215d95fa4
Ayush Goel <108970801+ayushgoel-01@users.noreply.github.com>
2025-08-18 10:14:04 +0530

    Time: 16 ms (57.93%) | Memory: 16.1 MB (55.73%) - LeetSync

--- 679-24-game/24-game.cpp
+++ 679-24-game/24-game.cpp
@@ -1,7 +1,7 @@
 class Solution {
 private:
     vector<double> getOperations(double a, double b){
-        vector<double> v;
+        vector<double> v;           // Store the result of all the possible operations
         v.push_back(a + b);
         v.push_back(a - b);
         v.push_back(b - a);
@@ -15,14 +15,13 @@ private:
     bool solve(vector<double>& cards){
         int n = cards.size();
 
-        if(n == 1){
+        if(n == 1){         // Check if it’s approximately 24 (within small epsilon, 1e-6).
             return fabs(cards[0] - 24.0) < 1e-6;
         }
 
         for(int i=0; i<n; i++){
-            for(int j=0; j<n; j++){
-                if(i == j) continue;
-
+            for(int j=i+1; j<n; j++){
+                
                 vector<double> next;
                 for(int k=0; k<n; k++){
                     if(k != i && k != j) next.push_back(cards[k]);
