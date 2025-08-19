d7c97a6eaba2a8c97f785804f61c2f8fa396ce7e
Ayush Goel <108970801+ayushgoel-01@users.noreply.github.com>
2025-08-19 10:38:25 +0530

    Time: 76 ms (70.10%) | Memory: 187.3 MB (57.47%) - LeetSync

--- 3791-fruits-into-baskets-iii/fruits-into-baskets-iii.cpp
+++ 3791-fruits-into-baskets-iii/fruits-into-baskets-iii.cpp
@@ -2,7 +2,7 @@ class Solution {
 private:
     void buildSegmentTree(int i, int l, int r, vector<int>& baskets, vector<int>& segTree){
         if(l == r){
-            segTree[i] = l;
+            segTree[i] = baskets[l];
             return;
         }
 
@@ -11,79 +11,42 @@ private:
         buildSegmentTree(2*i+1,l,mid,baskets,segTree);
         buildSegmentTree(2*i+2,mid+1,r,baskets,segTree);
 
-        int leftMaxIdx = segTree[2*i+1];
-        int rightMaxIdx = segTree[2*i+2];
-
-        segTree[i] = (baskets[leftMaxIdx] >= baskets[rightMaxIdx]) ? leftMaxIdx : rightMaxIdx;
+        segTree[i] = max(segTree[2*i+1],segTree[2*i+2]);
     }
 
-    int query(int start, int end, int i, int l, int r, vector<int>& baskets, vector<int>& segTree){
-        if(end < l || start > r) return -1;
-        if(start <= l && end >= r){
-            return segTree[i];
-        }
-
-        int mid = l + (r-l)/2;
-
-        int leftMaxIdx = query(start,end,2*i+1,l,mid,baskets,segTree);
-        int rightMaxIdx = query(start,end,2*i+2,mid+1,r,baskets,segTree);
-
-        if(leftMaxIdx == -1) return rightMaxIdx;
-        if(rightMaxIdx == -1) return leftMaxIdx;
-
-        return (baskets[leftMaxIdx] >= baskets[rightMaxIdx]) ? leftMaxIdx : rightMaxIdx;
-    }
-
-    void updateSegmentTree(int idx, int i, int l, int r, vector<int>& baskets, vector<int>& segTree){
+    bool query(int i, int l, int r, int fruit, vector<int>& segTree){
+        if(segTree[i] < fruit) return false;
         if(l == r){
             segTree[i] = -1;
-            return;
+            return true;
         }
 
         int mid = l + (r-l)/2;
 
-        if(idx <= mid){
-            updateSegmentTree(idx,2*i+1,l,mid,baskets,segTree);
+        bool placed = false;
+        if(segTree[2*i+1] >= fruit){
+            placed = query(2*i+1,l,mid,fruit,segTree);
         }
         else{
-            updateSegmentTree(idx,2*i+2,mid+1,r,baskets,segTree);
+            placed = query(2*i+2,mid+1,r,fruit,segTree);
         }
 
-        int leftMaxIdx = segTree[2*i+1];
-        int rightMaxIdx = segTree[2*i+2];
-
-        if(leftMaxIdx == -1) segTree[i] = rightMaxIdx;
-        else if(rightMaxIdx == -1) segTree[i] = leftMaxIdx;
-        else segTree[i] = (baskets[leftMaxIdx] >= baskets[rightMaxIdx]) ? leftMaxIdx : rightMaxIdx;
+        segTree[i] = max(segTree[2*i+1],segTree[2*i+2]);
+        return placed;
     }
 public:
     int numOfUnplacedFruits(vector<int>& fruits, vector<int>& baskets) {
         int n = fruits.size();
-        vector<int> segTree(4*n);
+        vector<int> segTree(4*n,-1);
 
         buildSegmentTree(0,0,n-1,baskets,segTree);
 
         int ans = 0;
         for(int i=0; i<n; i++){
-            int start = 0, end = n-1;
-            int idx = -1;
-            while(start <= end){
-                int mid = start + (end - start)/2;
-                int val = query(start,mid,0,0,n-1,baskets,segTree);
-
-                if(val != -1 && baskets[val] >= fruits[i]){
-                    idx = val;
-                    end = mid-1;
-                }
-                else start = mid+1;
-            }
-
-            if(idx != -1){
+            if(query(0,0,n-1,fruits[i],segTree) == false){
                 ans++;
-                updateSegmentTree(idx,0,0,n-1,baskets,segTree);
             }
         }
-
-        return n - ans;
+        return ans;
     }
 };
