58af044f4cc3193e4a4cd9f50bc57614427e21aa
Ayush Goel <108970801+ayushgoel-01@users.noreply.github.com>
2025-09-02 20:13:50 +0530

    Time: 0 ms (100.00%) | Memory: 32.6 MB (74.81%) - LeetSync

--- 3278-find-the-number-of-ways-to-place-people-i/find-the-number-of-ways-to-place-people-i.cpp
+++ 3278-find-the-number-of-ways-to-place-people-i/find-the-number-of-ways-to-place-people-i.cpp
@@ -1,21 +1,4 @@
 class Solution {
-private:
-    bool solve(int x1, int y1, int x2, int y2, unordered_set<int>& st1, unordered_set<int>& st2){
-        bool f1 = true, f2 = true;
-        for(int i=x1; i<=x2; i++){
-            if(st1.count(i) >= 1){
-                f1 = false;
-                break;
-            }
-        }
-        for(int j=y2; j<=y1; j++){
-            if(st2.count(j) >= 1){
-                f2 = false;
-                break;
-            }
-        }
-        return f1 || f2;
-    }
 public:
     int numberOfPairs(vector<vector<int>>& points) {
         int n = points.size();
@@ -31,15 +14,17 @@ public:
         for(int i=0; i<n; i++){
             int x1 = points[i][0];
             int y1 = points[i][1];
+            int maxY = INT_MIN;
 
-            unordered_set<int> st1, st2;
             for(int j=i+1; j<n; j++){
                 int x2 = points[j][0];
                 int y2 = points[j][1];
 
-                if(x1 <= x2 && y1 >= y2 && solve(x1,y1,x2,y2,st1,st2)) ans++;
-                st1.insert(x2);
-                st2.insert(y2);
+                if(y2 > y1) continue;
+                if(y2 > maxY){
+                    ans++;
+                    maxY = y2;
+                }
             }
         }
         return ans;
