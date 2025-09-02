92f86b2247e6b134b6c1dce0b0ec806adc80365e
Ayush Goel <108970801+ayushgoel-01@users.noreply.github.com>
2025-09-02 09:53:00 +0530

    Time: 143 ms (8.89%) | Memory: 53.4 MB (7.41%) - LeetSync

---
+++ 3278-find-the-number-of-ways-to-place-people-i/find-the-number-of-ways-to-place-people-i.cpp
@@ -0,0 +1,52 @@
+class Solution {
+private:
+    bool solve(int x1, int y1, int x2, int y2, unordered_set<int>& st1, unordered_set<int>& st2){
+        bool f1 = true, f2 = true;
+        for(int i=x1; i<=x2; i++){
+            if(st1.count(i) >= 1){
+                f1 = false;
+                break;
+            }
+        }
+        for(int j=y2; j<=y1; j++){
+            if(st2.count(j) >= 1){
+                f2 = false;
+                break;
+            }
+        }
+        return f1 || f2;
+    }
+public:
+    int numberOfPairs(vector<vector<int>>& points) {
+        int n = points.size();
+
+        auto cmp = [&](vector<int>& a, vector<int>& b){
+            if(a[0] == b[0]) return a[1] > b[1];
+            return a[0] < b[0];
+        };
+        sort(points.begin(),points.end(),cmp);
+
+        for(auto it: points){
+            cout<<it[0]<<","<<it[1]<<"  ";
+        }
+
+        int ans = 0;
+
+        for(int i=0; i<n; i++){
+            int x1 = points[i][0];
+            int y1 = points[i][1];
+
+            unordered_set<int> st1, st2;
+            for(int j=i+1; j<n; j++){
+                int x2 = points[j][0];
+                int y2 = points[j][1];
+
+                if(x1 <= x2 && y1 >= y2 && solve(x1,y1,x2,y2,st1,st2)) ans++;
+                st1.insert(x2);
+                st2.insert(y2);
+            }
+        }
+
+        return ans;
+    }
+};
