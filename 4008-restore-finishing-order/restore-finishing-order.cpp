95bb2850c99b5ff5bf98045058e31dda8db0145c
Ayush Goel <108970801+ayushgoel-01@users.noreply.github.com>
2025-09-04 12:52:41 +0530

    Time: 4 ms (25.48%) | Memory: 50.3 MB (72.58%) - LeetSync

---
+++ 4008-restore-finishing-order/restore-finishing-order.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    vector<int> recoverOrder(vector<int>& order, vector<int>& friends) {
+        unordered_set<int> st(friends.begin(),friends.end());
+
+        vector<int> ans;
+        for(auto i: order){
+            if(st.count(i)) ans.push_back(i);
+        }
+        return ans;
+    }
+};
