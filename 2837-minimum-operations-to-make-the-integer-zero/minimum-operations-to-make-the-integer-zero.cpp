2b7d0915692dde09864014822db7337e2c546f33
Ayush Goel <108970801+ayushgoel-01@users.noreply.github.com>
2025-09-05 19:34:16 +0530

    Time: 0 ms (100.00%) | Memory: 8.2 MB (52.07%) - LeetSync

--- 2837-minimum-operations-to-make-the-integer-zero/minimum-operations-to-make-the-integer-zero.cpp
+++ 2837-minimum-operations-to-make-the-integer-zero/minimum-operations-to-make-the-integer-zero.cpp
@@ -7,9 +7,22 @@ public:
 
             if(val < 0) return -1;
 
+            // Calculate no. of set bits
             if(__builtin_popcountll(val) <= t && t <= val) return t;
             t++;
         }
-        return -1;
+        return -1; 
     }
 };
+
+/*  Simplify the equation:- 
+    num11 = num1 - 2^i1 + num2
+    num12 = num11 - 2^i2 + num2  =>  num12 = (num1 - 2^i1 + num2) - 2^i2 + num2
+    num13 = num12 - 2^i3 + num2  =>  num13 = ((num1 - 2^i1 + num2) - 2^i2 + num2) - 2^i3 + num2
+
+    0 = num1 - (2^i1 + 2^i2 + 2^i3 + ... 2^it) - t*num2
+    num1 - t*num2 = 2^i1 + 2^i2 + 2^i3 + ... 2^it
+    val = sum of t powers of 2
+
+To find that in how many powers of 2 a number can be represented, just calculate no. of set bits
+*/
