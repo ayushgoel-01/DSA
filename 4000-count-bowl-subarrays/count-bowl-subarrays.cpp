c5b8775943d044033d4c98fc6a21f5440dd29b84
Ayush Goel <108970801+ayushgoel-01@users.noreply.github.com>
2025-09-07 12:07:44 +0530

    Time: 57 ms (7.14%) | Memory: 140 MB (7.14%) - LeetSync

---
+++ 4000-count-bowl-subarrays/count-bowl-subarrays.cpp
@@ -0,0 +1,49 @@
+class Solution {
+private:
+    vector<int> nextGreaterRight(vector<int>& nums, int n){
+        vector<int> ans(n,-1);
+        stack<int> st;
+
+        for(int i=0; i<n; i++){
+            while(!st.empty() && nums[i] >= nums[st.top()]){
+                ans[st.top()] = i;
+                st.pop();
+            }
+            st.push(i);
+        }
+        return ans;
+    }
+    vector<int> nextGreaterLeft(vector<int>& nums, int n){
+        vector<int> ans(n,-1);
+        stack<int> st;
+
+        for(int i=0; i<n; i++){
+            while(!st.empty() && nums[i] > nums[st.top()]){
+                st.pop();
+            }
+            if(!st.empty()) ans[i] = st.top();
+            st.push(i);
+        }
+        return ans;
+    }
+public:
+    long long bowlSubarrays(vector<int>& nums) {
+        int n = nums.size();
+
+        vector<int> left = nextGreaterLeft(nums,n);
+        vector<int> right = nextGreaterRight(nums,n);
+
+        long long ans = 0;
+        for(int i=0; i<n; i++){
+            if(i > 0){
+                int l = left[i];
+                if(l != -1 && i-l+1 >= 3) ans++;
+            }
+            if(i < n-1){
+                int r = right[i];
+                if(r != -1 && r-i+1 >= 3) ans++;
+            }
+        }
+        return ans;
+    }
+};
