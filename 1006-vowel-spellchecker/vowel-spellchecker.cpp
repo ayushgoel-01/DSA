24d2a8bd7db04fee7f0e81d837dfddcb1758d33f
Ayush Goel <108970801+ayushgoel-01@users.noreply.github.com>
2025-09-14 13:28:59 +0530

    Time: 36 ms (77.22%) | Memory: 40.5 MB (76.58%) - LeetSync

--- 1006-vowel-spellchecker/vowel-spellchecker.cpp
+++ 1006-vowel-spellchecker/vowel-spellchecker.cpp
@@ -1,17 +1,17 @@
 class Solution {
 private:
     bool isVowel(char ch){
-        return ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' ||
-                ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U';
+        return ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' || ch == 'A' || 
+                ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U';
     }
 
     string toLowerCase(string& s){
         int n = s.size();
         string ans = "";
 
-        for(int i=0; i<n; i++){
-            if(s[i] >= 'a' && s[i] <= 'z') ans += s[i];
-            else ans += s[i] - 'A' + 'a';
+        for(auto i: s){
+            if(i >= 'a' && i <= 'z') ans += i;
+            else ans += i - 'A' + 'a';
         }
         return ans;
     }
@@ -19,6 +19,7 @@ private:
     string toWildCard(string& s){
         string tmp = toLowerCase(s);
         string ans = "";
+
         for(auto i: tmp){
             if(isVowel(i)) ans += "*";
             else ans += i;
@@ -26,18 +27,17 @@ private:
         return ans;
     }
 
-    void makeMapping(vector<string>& v, int n, unordered_map<string,string>& mp1, unordered_map<string,string>& mp2){
+    void makeMapping(vector<string>& v, int n, unordered_map<string,string>& mp1,
+        unordered_map<string,string>& mp2){
         for(int i=0; i<n; i++){
             string s = v[i];
             string temp = toLowerCase(s);
-            
             if(mp1.find(temp) == mp1.end()) mp1[temp] = s;
         }
 
         for(int i=0; i<n; i++){
             string s = v[i];
             string temp = toWildCard(s);
-
             if(mp2.find(temp) == mp2.end()) mp2[temp] = s;
         }
     }
@@ -49,9 +49,10 @@ public:
 
         unordered_map<string,string> mp1, mp2;
         unordered_set<string> st(wordlist.begin(),wordlist.end());
-        makeMapping(wordlist,n,mp1,mp2);
 
+        makeMapping(wordlist,n,mp1,mp2);
         vector<string> ans(m,"");
+        
         for(int i=0; i<m; i++){
             string l1 = toLowerCase(queries[i]);
             string l2 = toWildCard(queries[i]);
