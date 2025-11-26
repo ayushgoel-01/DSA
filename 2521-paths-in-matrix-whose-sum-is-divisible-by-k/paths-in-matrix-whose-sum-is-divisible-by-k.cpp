aadddd96c2fd7e4d172cfb170a6b25ccee446c75
Ayush Goel <108970801+ayushgoel-01@users.noreply.github.com>
2025-11-26 09:41:50 +0530

    Time: 303 ms (22.04%) | Memory: 139.9 MB (37.68%) - LeetSync

---
+++ 2521-paths-in-matrix-whose-sum-is-divisible-by-k/paths-in-matrix-whose-sum-is-divisible-by-k.cpp
@@ -0,0 +1,27 @@
+class Solution {
+int mod = 1e9 + 7;
+private:
+    int solve(int i, int j, int sum, vector<vector<int>>& grid, int n, int m, int k,
+    vector<vector<vector<int>>>& dp){
+        if(i == n-1 && j == m-1){
+            sum = (sum + grid[i][j])%k;
+            return (sum == 0);
+        }
+        if(i >= n || j >= m) return 0;
+
+        if(dp[i][j][sum] != -1) return dp[i][j][sum];
+
+        long long right = solve(i,j+1,(sum+grid[i][j])%k,grid,n,m,k,dp)%mod;
+        long long down = solve(i+1,j,(sum+grid[i][j])%k,grid,n,m,k,dp)%mod;
+
+        return dp[i][j][sum] = (right%mod + down%mod)%mod;
+    }
+public:
+    int numberOfPaths(vector<vector<int>>& grid, int k) {
+        int n = grid.size();
+        int m = grid[0].size();
+
+        vector<vector<vector<int>>> dp(n, vector<vector<int>>(m, vector<int>(k,-1)));
+        return solve(0,0,0,grid,n,m,k,dp);
+    }
+};
