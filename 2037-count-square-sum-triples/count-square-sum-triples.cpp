59a26235aa792f8f73f8b2314455ffb07c916352
Ayush Goel <108970801+ayushgoel-01@users.noreply.github.com>
2025-12-08 10:20:29 +0530

    Time: 193 ms (21.68%) | Memory: 7.9 MB (55.75%) - LeetSync

---
+++ 2037-count-square-sum-triples/count-square-sum-triples.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int countTriples(int n) {
+        int ans = 0;
+        
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=n; j++){
+                for(int k=1; k<=n; k++){
+                    long long val1 = i * 1LL * i;
+                    long long val2 = j * 1LL * j;
+                    long long val3 = k * 1LL * k;
+
+                    if(val1 + val2 == val3) ans++;
+                }
+            }
+        }
+        return ans;
+    }
+};
