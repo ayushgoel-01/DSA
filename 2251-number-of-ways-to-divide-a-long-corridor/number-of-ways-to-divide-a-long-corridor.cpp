dcf872858884b53bbb73e2327e670817d6a336c0
Ayush Goel <108970801+ayushgoel-01@users.noreply.github.com>
2025-12-14 20:06:38 +0530

    Time: 43 ms (23.50%) | Memory: 58.6 MB (40.00%) - LeetSync

--- 2251-number-of-ways-to-divide-a-long-corridor/number-of-ways-to-divide-a-long-corridor.cpp
+++ 2251-number-of-ways-to-divide-a-long-corridor/number-of-ways-to-divide-a-long-corridor.cpp
@@ -4,20 +4,17 @@ public:
     int numberOfWays(string corridor) {
         int n = corridor.size();
         
-        int cnt = 0;
-        for(auto i: corridor) cnt += (i == 'S');
-
-        if(cnt % 2 == 1 || cnt <= 1) return 0;
-        
         vector<int> v;
         for(int i=0; i<n; i++){
             if(corridor[i] == 'S') v.push_back(i);
         }
 
+        int size = v.size();
+        if(size % 2 == 1 || size == 0) return 0;
+
         int ans = 1;
         for(int i=2; i<v.size(); i+=2){
-            if(ans == 1) ans = v[i] - v[i-1];
-            else ans = (ans * 1LL * (v[i]-v[i-1])%mod)%mod;
+            ans = (ans * 1LL * (v[i]-v[i-1])%mod)%mod;
         }
         return ans;
     }
