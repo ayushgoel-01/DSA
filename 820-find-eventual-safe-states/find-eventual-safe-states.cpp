425bb06302f4ed0dbe858724a916a52ae596f6b0
Ayush Goel <108970801+ayushgoel-01@users.noreply.github.com>
2025-12-24 18:05:15 +0530

    Time: 4 ms (90.00%) | Memory: 51.4 MB (94.51%) - LeetSync

--- 820-find-eventual-safe-states/find-eventual-safe-states.cpp
+++ 820-find-eventual-safe-states/find-eventual-safe-states.cpp
@@ -1,31 +1,41 @@
-class Solution {            // DFS
+class Solution {
 private:
-    bool detectCycle(int node, vector<vector<int>>& graph, vector<bool>& vis, vector<bool>& inRecursion){
+    bool solve(int node, vector<vector<int>>& adj, vector<bool>& vis, vector<bool>& pathVis,
+    vector<bool>& check){
         vis[node] = true;
-        inRecursion[node] = true;
+        pathVis[node] = true;
 
-        // Check for Cycle
-        for(auto& curr: graph[node]){
-            if(!vis[curr] && detectCycle(curr,graph,vis,inRecursion)) return true;
-            else if(inRecursion[curr] == true) return true;
+        for(auto it: adj[node]){
+            if(!vis[it]){
+                if(solve(it,adj,vis,pathVis,check)){
+                    check[node] = false;
+                    return true;
+                }
+            }
+            else if(pathVis[it]){
+                check[node] = false;
+                return true;
+            }
         }
 
-        inRecursion[node] = false;
+        pathVis[node] = false;
+        check[node] = true;
         return false;
     }
 public:
     vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
         int n = graph.size();
 
-        vector<bool> vis(n,false), inRecursion(n,false);
-
+        vector<bool> vis(n,false), pathVis(n,false), check(n,false);
         for(int i=0; i<n; i++){
-            if(!vis[i]) detectCycle(i,graph,vis,inRecursion);
+            if(!vis[i]){
+                solve(i,graph,vis,pathVis,check);
+            }
         }
 
         vector<int> ans;
-        for(int i=0; i<n; i++){         // Nodes which are not part of Cycle
-            if(!inRecursion[i]) ans.push_back(i);
+        for(int i=0; i<n; i++){
+            if(check[i]) ans.push_back(i);
         }
         return ans;
     }
