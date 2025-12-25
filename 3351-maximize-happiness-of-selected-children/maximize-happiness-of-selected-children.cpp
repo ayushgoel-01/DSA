8adfd2e4bafe369cf17db257bdbaa92165b4e196
Ayush Goel <108970801+ayushgoel-01@users.noreply.github.com>
2025-12-25 16:19:39 +0530

    Time: 219 ms (13.83%) | Memory: 110 MB (29.08%) - LeetSync

---
+++ 3351-maximize-happiness-of-selected-children/maximize-happiness-of-selected-children.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    long long maximumHappinessSum(vector<int>& happiness, int k) {
+        int n = happiness.size();
+
+        sort(happiness.rbegin(),happiness.rend());
+
+        long long ans = 0, cnt = 0;
+        for(int i=0; i<k; i++){
+            happiness[i] -= cnt;
+            cnt++;
+            if(happiness[i] <= 0) happiness[i] = 0;
+        }
+
+        for(int i=k; i<n; i++){
+            happiness[i] -= k;
+            if(happiness[i] <= 0) happiness[i] = 0;
+        }
+
+        for(int i=0; i<k; i++){
+            ans += happiness[i];
+        }
+        return ans;
+    }
+};
