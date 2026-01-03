8fed98834d9b4abdb07868aaa788829958af5380
Ayush Goel <108970801+ayushgoel-01@users.noreply.github.com>
2026-01-03 19:26:54 +0530

    Time: 59 ms (26.67%) | Memory: 18.4 MB (20.00%) - LeetSync

--- 1527-number-of-ways-to-paint-n-3-grid/number-of-ways-to-paint-n-3-grid.cpp
+++ 1527-number-of-ways-to-paint-n-3-grid/number-of-ways-to-paint-n-3-grid.cpp
@@ -1,41 +1,36 @@
 class Solution {
-int M = 1e9 + 7;
-vector<vector<int>> dp;
+int mod = 1e9 + 7;
 private:
-    int solve(int n, int prevIdx, vector<string>& states){
+    int solve(int n, int prev, vector<string>& v, vector<vector<int>>& dp){
         if(n == 0) return 1;
 
-        if(dp[n][prevIdx] != -1) return dp[n][prevIdx];
+        if(dp[n][prev] != -1) return dp[n][prev];
 
-        string prev = states[prevIdx];
-
-        int ways = 0;
-        for(int i=0; i<12; i++){
-            if(i == prevIdx) continue;
-
-            string curr = states[i];
-            bool flag = true;
+        int ans = 0;
+        for(int curr=0; curr<12; curr++){
+            if(prev == curr) continue;
+            bool flag = false;
 
             for(int j=0; j<3; j++){
-                if(prev[j] == curr[j]){
-                    flag = false;
+                if(v[prev][j] == v[curr][j]){
+                    flag = true;
                     break;
                 }
             }
-            if(flag) ways = (ways + solve(n-1,i,states)) % M;
+            if(!flag){
+                ans = (ans + solve(n-1,curr,v,dp))%mod;
+            }
         }
-        return dp[n][prevIdx] = ways;
+        return dp[n][prev] = ans;
     }
 public:
     int numOfWays(int n) {
-        vector<string> states = {"RYG","RGY","RYR","RGR","YRG","YGR","YGY","YRY","GYR","GRY",
-        "GRG", "GYG"};
+        vector<string> v = {"RYG","RYR","RGR","RGY","YRG","YRY","YGR","YGY","GRY","GYR","GYG","GRG"};
 
         int ans = 0;
-        dp = vector<vector<int>>(n+1,vector<int>(12,-1));
-
+        vector<vector<int>> dp(n+1, vector<int>(13,-1));
         for(int i=0; i<12; i++){
-            ans = (ans + solve(n-1,i,states)) % M;
+            ans = (ans + solve(n-1,i,v,dp))%mod;
         }
         return ans;
     }
