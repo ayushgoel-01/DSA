79ec1fe2205fdb9442899b65a38138e4eeffdbd8
Ayush Goel <108970801+ayushgoel-01@users.noreply.github.com>
2026-01-15 19:49:08 +0530

    Time: 0 ms (100.00%) | Memory: 31.8 MB (67.83%) - LeetSync

---
+++ 3214-maximize-area-of-square-hole-in-grid/maximize-area-of-square-hole-in-grid.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int maximizeSquareHoleArea(int n, int m, vector<int>& hBars, vector<int>& vBars) {
+        int size1 = hBars.size();
+        int size2 = vBars.size();
+
+        sort(hBars.begin(),hBars.end());
+        sort(vBars.begin(),vBars.end());
+
+        int maxH = 1, maxV = 1;
+
+        int curr = 1;
+        for(int i=1; i<size1; i++){
+            if(hBars[i] == hBars[i-1] + 1){
+                curr++;
+                maxH = max(maxH,curr);
+            }
+            else curr = 1;
+        }
+
+        curr = 1;
+        for(int i=1; i<size2; i++){
+            if(vBars[i] == vBars[i-1] + 1){
+                curr++;
+                maxV = max(maxV,curr);
+            }
+            else curr = 1;
+        }
+
+        int val = min(maxH+1,maxV+1);
+        return val * val;
+    }
+};
