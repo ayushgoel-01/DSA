44637b1b687c1a23a0418b261edbfd933def4c50
Ayush Goel <108970801+ayushgoel-01@users.noreply.github.com>
2026-01-19 14:25:15 +0530

    Time: 54 ms (22.73%) | Memory: 32.9 MB (12.27%) - LeetSync

---
+++ 1413-maximum-side-length-of-a-square-with-sum-less-than-or-equal-to-threshold/maximum-side-length-of-a-square-with-sum-less-than-or-equal-to-threshold.cpp
@@ -0,0 +1,39 @@
+class Solution {
+private:
+    int solve(vector<vector<int>>& pref, int i, int j, int r2, int c2){
+        int sum = pref[r2][c2];
+        if(i > 0) sum -= pref[i-1][c2];
+        if(j > 0) sum -= pref[r2][j-1];
+        if(i > 0 && j > 0) sum += pref[i-1][j-1];
+
+        return sum;
+    }
+public:
+    int maxSideLength(vector<vector<int>>& mat, int threshold) {
+        int n = mat.size();
+        int m = mat[0].size();
+
+        vector<vector<int>> pref(n, vector<int>(m,0));
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                pref[i][j] = mat[i][j] + (i > 0 ? pref[i-1][j] : 0) + (j > 0 ? pref[i][j-1] : 0)
+                             - ((i > 0 && j > 0) ? pref[i-1][j-1] : 0);
+            }
+        }
+
+        int best = 0;
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                for(int side=0; side<min(n-i,m-j); side++){
+                    int r2 = i + side;
+                    int c2 = j + side;
+
+                    int sum = solve(pref,i,j,r2,c2);
+                    if(sum <= threshold) best = max(best,side+1);
+                    else break;
+                }
+            }
+        }
+        return best;
+    }
+};
