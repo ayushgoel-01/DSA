8d8f376c2914f3a68a55fd9987e07ecc07a7588b
Ayush Goel <108970801+ayushgoel-01@users.noreply.github.com>
2026-04-30 09:33:19 +0530

    Time: 806 ms (70.04%) | Memory: 499.5 MB (46.52%) - LeetSync

--- 3986-maximum-path-score-in-a-grid/maximum-path-score-in-a-grid.cpp
+++ 3986-maximum-path-score-in-a-grid/maximum-path-score-in-a-grid.cpp
@@ -1,30 +1,34 @@
 class Solution {
 private:
     int n, m;
-    int solve(int i, int j, vector<vector<int>>& grid, int k, vector<vector<vector<int>>>& dp){
-        if(i >= n || j >= m) return -1e8;
-
-        int cost = (grid[i][j] == 0 ? 0 : 1);
-        if(k < cost) return -1e8;
-
-        if(i == n-1 && j == m-1) return grid[i][j];
+    int solve(int i, int j, vector<vector<int>>& grid, int k, vector<vector<vector<int>>>& dp) {
+        if(i == n-1 && j == m-1){
+            if(k - (grid[i][j] == 0 ? 0 : 1) >= 0) return grid[i][j];
+            return -1e9;
+        }
+        if(i >= n || j >= m || k < 0) return -1e9;
 
         if(dp[i][j][k] != -1) return dp[i][j][k];
 
-        int op1 = solve(i+1,j,grid,k-cost,dp);
-        int op2 = solve(i,j+1,grid,k-cost,dp);
+        int cost = grid[i][j] == 0 ? 0 : 1;
 
-        int best = max(op1,op2);
+        int op1 = -1e9;
+        if (k - cost >= 0) {
+            op1 = grid[i][j] + solve(i+1,j,grid,k-cost,dp);
+        }
 
-        if(best < 0) return dp[i][j][k] = -1e8;
-        return dp[i][j][k] = grid[i][j] + best;
+        int op2 = -1e9;
+        if (k - cost >= 0) {
+            op2 = grid[i][j] + solve(i,j+1,grid,k-cost,dp);
+        }
+        return dp[i][j][k] = max(op1,op2);
     }
 
 public:
     int maxPathScore(vector<vector<int>>& grid, int k) {
         n = grid.size();
         m = grid[0].size();
-        vector<vector<vector<int>>> dp(n, vector<vector<int>>(m, vector<int>(k+1, -1)));
+        vector<vector<vector<int>>> dp(n, vector<vector<int>>(m, vector<int>(k+1,-1)));
 
         int ans = solve(0,0,grid,k,dp);
         return ans < 0 ? -1 : ans;
